ccd33526bb954468a10c4a91e6e5189a21e2f0b7
Rohit Gautam <gautamrohit55@gmail.com>
2021-07-17 19:07:21 +0530

    intial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import Carousel from "./Components/Carousel";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Carousel />
+    </div>
+  );
+}
+
+export default App;
